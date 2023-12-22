fe1ef4af4fc43872d023694176e4e77022104c0c
Yash Seth <71393551+yash-seth@users.noreply.github.com>
2023-12-22 12:59:20 +0530

    feat: Update test.py to support launch from container

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 origins = [
     "http://127.0.0.1",
-    "http://127.0.0.1:8000",
+    "http://127.0.0.1:80",
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -85,4 +85,4 @@ async def predict(
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
